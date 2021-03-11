896516cfd00af42b3503feef53c25777a15b9b1c
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-03-11 10:51:15 -0500

    askBCS

--- Develop/script.js
+++ Develop/script.js
@@ -7,35 +7,48 @@ var numberCharacters;
 var specialCharacters;
 var passwordLength;
 
+var upperArr = ["A","B","C","D","E","F","G","H","I","J"];
+var lowerArr = ["a","b","c","d","e","f","g","h","i","j"];
+var numberArr =[0,1,2,3,4,5,6,7,8,9];
+var specialArr = ["!","@","#","$","%","&","?","<",">","+"];
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 generateBtn.addEventListener("click", function() {
   alert("Please select whether you would like to include: UPPERCASE, LOWERCASE, NUMBERS and SPECIAL CHARACTERS"); 
   upperCase = confirm("Would you like to include UPPERCASE characters in your password?");
-  if (upperCase === "true") {
+  if (upperCase) {
     alert("You chose to include UPPERCASE");
-  } esle { alert("You chose not to include UPPERCASE");}
+  } else { alert("You chose not to include UPPERCASE");}
 
    lowerCase = confirm("Would you like to include LOWERCASE characters in your password?");
-   if (lowerCase ==="true"){
+   if (lowerCase){
     alert("You chose to include LOWERCASE");
-  } esle { alert("You chose not to include LOWERCASE");}
+  } else { alert("You chose not to include LOWERCASE");}
 
    numberCharacters = confirm("Would you like to include NUMBERS characters in your password?");
-   if (numberCharacters === "true"){
+   if (numberCharacters){
     alert("You chose to include NUMBERS");
-  } esle { alert("You chose not to include NUMBERS");}
+  } else { alert("You chose not to include NUMBERS");}
 
-   specialCharacters = confirm("Would you like to include SPECIAL characters in your password?");
-   if (specialCharacters == "true"){
+   specialCharacters = confirm("Would you like to include SPECIAL CHARACTERS in your password?");
+   if (specialCharacters){
     alert("You chose to include SPECIAL CHARACTERS");
-  } esle { alert("You chose not to include SPECIAL CHARACTERS");}
+  } else { alert("You chose not to include SPECIAL CHARACTERS");}
 
-   passwordLength = prompt("Choose your desired password length between 8 and 128");
+   var length = function() {
+     passwordLength = prompt("Choose your desired password length between 8 and 128");
+    console.log(passwordLength);
+    if (passwordLength < 8 || paswordLength > 128) {
+      alert("Please choose choose a number between 8 and 128");
+     length();
+    }
+    };
+   
 },false);
 
-console.log(upperCase);
+
 
 
 
