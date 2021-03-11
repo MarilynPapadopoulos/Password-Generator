5741b61d6ca25a1aaf89d861fdaa9c7046dc9f91
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-03-11 12:56:09 -0500

    AskBCS

--- Develop/script.js
+++ Develop/script.js
@@ -7,12 +7,14 @@ var numberCharacters;
 var specialCharacters;
 var passwordLength;
 
+var choices=0;
+
 var upperArr = ["A","B","C","D","E","F","G","H","I","J"];
 var lowerArr = ["a","b","c","d","e","f","g","h","i","j"];
 var numberArr =[0,1,2,3,4,5,6,7,8,9];
 var specialArr = ["!","@","#","$","%","&","?","<",">","+"];
 
-// Get references to the #generate element
+// Get references to the #generate (button) element 
 var generateBtn = document.querySelector("#generate");
 
 generateBtn.addEventListener("click", function() {
@@ -20,31 +22,42 @@ generateBtn.addEventListener("click", function() {
   upperCase = confirm("Would you like to include UPPERCASE characters in your password?");
   if (upperCase) {
     alert("You chose to include UPPERCASE");
+    
+    console.log(choices);
   } else { alert("You chose not to include UPPERCASE");}
 
    lowerCase = confirm("Would you like to include LOWERCASE characters in your password?");
    if (lowerCase){
     alert("You chose to include LOWERCASE");
+    
+    console.log(choices);
   } else { alert("You chose not to include LOWERCASE");}
 
    numberCharacters = confirm("Would you like to include NUMBERS characters in your password?");
    if (numberCharacters){
     alert("You chose to include NUMBERS");
+    
+    console.log(choices);
   } else { alert("You chose not to include NUMBERS");}
 
    specialCharacters = confirm("Would you like to include SPECIAL CHARACTERS in your password?");
    if (specialCharacters){
     alert("You chose to include SPECIAL CHARACTERS");
-  } else { alert("You chose not to include SPECIAL CHARACTERS");}
+     
+    console.log(choices);
+  } 
+  else { alert("You chose not to include SPECIAL CHARACTERS");}
 
    var length = function() {
      passwordLength = prompt("Choose your desired password length between 8 and 128");
-    console.log(passwordLength);
-    if (passwordLength < 8 || paswordLength > 128) {
+     passwordLength = parseInt(passwordLength);
+     console.log(passwordLenght);
+
+     if (passwordLength < 8 && paswordLength > 128) {
       alert("Please choose choose a number between 8 and 128");
-     length();
-    }
+      }
     };
+    length();
    
 },false);
 
@@ -54,6 +67,17 @@ generateBtn.addEventListener("click", function() {
 
 // Write password to the #password input
 //function writePassword() {
+
+// set the lenght of the passwordArray to passwordLength
+
+
+//   for (var i = 0; i >=  passwordLength, i++)  {
+//     if (upperCase){
+//       add upperArr[i] to the password;
+//       password.push 
+//   }
+  
+// }
   //var password = generatePassword();
   //var passwordText = document.querySelector("#password");
 
